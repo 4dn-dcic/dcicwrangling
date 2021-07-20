54675cc2cb1a20943453562d307fe756b694e423
andreacosolo <andrea_cosolo@hms.harvard.edu>
2021-07-20 18:57:44 -0400

    combine processed_files and other_processed_files

--- functions/geo_minimization.py
+++ functions/geo_minimization.py
@@ -4,24 +4,20 @@ Common functions for GEO Minimization of the JSON objects
 URL = 'https://data.4dnucleome.org'
 
 
-def num2str(value):
-    '''transform number to string'''
-    if isinstance(value, (int, float)):
-        return str(value)
-    else:
-        return value
-
-
 def add_to_output_dict(key, value, output_dictionary):
-    '''add to the output_dictionary either a single key:value pair or
-    all the key:values if value is a dictionary'''
+    '''Add to the output_dictionary either a single key:value pair or
+    all the key:values if value is a dictionary.
+    If a key is already in the output_dictionary, add value to the existing one
+    '''
+    def _add_to_output_dict(k, v):
+        if v:  # skip if None
+            output_dictionary[k] = output_dictionary[k] + v if output_dictionary.get(k) else v
+        return
     if isinstance(value, dict):
         for inner_key, inner_value in value.items():
-            if inner_value:  # skip if None
-                output_dictionary[inner_key] = num2str(inner_value)
+            _add_to_output_dict(inner_key, inner_value)
     else:
-        if value:  # skip if None
-            output_dictionary[key] = num2str(value)
+        _add_to_output_dict(key, value)
     return output_dictionary
 
 
